04bf3cd4abf75e8dc6948cf91502924d93b1e23f
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2021-12-10 20:54:26 +0900

    WIP - normalize expressions by "lifting" commands

---
+++ src/static/normalize.ts
@@ -0,0 +1,48 @@
+import { Writer } from '../monad/writer'
+import { Prog, Cmd, Expr } from './ast'
+import { visitM } from './transformM'
+
+let temp = 0
+const makeTemp = (): string => `__temp__${temp++}`
+
+//-----------------------------------------------------------------------------
+// Normalization
+//-----------------------------------------------------------------------------
+
+const transformer = visitM(Writer.of)({
+  Expr: {
+    'Expr.Cmd': ({ cmd }) => {
+      const [c, cmds] = normalizeCmd(cmd).run()
+      return Writer.of(Expr.Cmds([...cmds, c]))
+    },
+    'Expr.Cmds': ({ cmds }) => {
+      const commands = normalizeCmds(cmds)
+      return Writer.of(Expr.Cmds(commands))
+    },
+    'Expr.Result': ({ cmdExpr }) => {
+      const cmdExprM = transformer.Expr(cmdExpr)
+      return cmdExprM.flatMap((cmdExpr) => {
+        const temp = makeTemp()
+        const def = Cmd.Def({ variable: temp, value: Expr.Result(cmdExpr) })
+        return Writer.tell(def).flatMap(() => Writer.of(Expr.Var(temp)))
+      })
+    },
+    // TODO: If any branch contains a Expr.Result(cmdExpr),
+    // promote all branches to cmd with 'Cmd.Return'.
+    // Lift Result outside of Expr.Cond
+    'Expr.Cond': (e) => Writer.of<Cmd, Expr>(e),
+  },
+})
+
+const normalizeCmd = (cmd: Cmd) => transformer.Cmd(cmd) as Writer<Cmd, Cmd>
+
+const normalizeCmds = (cmds: Cmd[]): Cmd[] =>
+  cmds.map(normalizeCmd).flatMap((writer) => {
+    const [cmd, cmds] = writer.run()
+    return [...cmds, cmd]
+  })
+
+export const normalize = (program: Prog): Prog => {
+  const { commands } = program
+  return { commands: normalizeCmds(commands) }
+}
--- src/static/transformM.ts
+++ src/static/transformM.ts
@@ -11,39 +11,38 @@ import {
 
 interface Monad<T> {
   map<S>(f: (t: T) => S): Monad<S>
-  flatten<S>(): Monad<S>
   flatMap<S>(f: (t: T) => Monad<S>): Monad<S>
 }
 
-export const transformM = (of: <T>(t: T) => Monad<T>) => {
+type Transformer = {
+  Cmd: (cmd: Cmd) => Monad<Cmd>
+  Expr: (expr: Expr) => Monad<Expr>
+  Branch: (branch: any) => Monad<any>
+}
+type CmdVisitor = Matcher<Cmd, Monad<Cmd>>
+type ExprVisitor = Matcher<Expr, Monad<Expr>>
+type BranchVisitor = {
+  'Branch.Choice': (branch: ChoiceBranch) => Monad<ChoiceBranch>
+  'Branch.Fork': (branch: ForkBranch) => Monad<ForkBranch>
+  'Branch.Cond': (branch: CondBranch) => Monad<CondBranch>
+}
+type ASTVisitor = {
+  Cmd?: Partial<CmdVisitor>
+  Expr?: Partial<ExprVisitor>
+  Branch?: Partial<BranchVisitor>
+}
+
+export const visitM = (pure: <T>(t: T) => Monad<T>) => {
   const sequenceM = <T>(arrM: Monad<T>[]): Monad<T[]> =>
     arrM.reduce(
       (arrM, valM) =>
-        arrM.flatMap((arr) => valM.flatMap((val) => of([...arr, val]))),
-      of([] as T[])
+        arrM.flatMap((arr) => valM.flatMap((val) => pure([...arr, val]))),
+      pure([] as T[])
     )
-
-  type Transformer = {
-    Cmd: (cmd: Cmd) => Monad<Cmd>
-    Expr: (expr: Expr) => Monad<Expr>
-    Branch: (branch: any) => Monad<any>
-  }
-  type CmdVisitor = Matcher<Cmd, Monad<Cmd>>
-  type ExprVisitor = Matcher<Expr, Monad<Expr>>
-  type BranchVisitor = {
-    'Branch.Choice': (branch: ChoiceBranch) => Monad<ChoiceBranch>
-    'Branch.Fork': (branch: ForkBranch) => Monad<ForkBranch>
-    'Branch.Cond': (branch: CondBranch) => Monad<CondBranch>
-  }
-  type ASTVisitor = {
-    Cmd?: Partial<CmdVisitor>
-    Expr?: Partial<ExprVisitor>
-    Branch?: Partial<BranchVisitor>
-  }
 
   // Commands
   const visitCmd = (transformer: Transformer): CmdVisitor => ({
-    'Cmd.Exec': ({ fn, args }) => of(Cmd.Exec({ fn, args })),
+    'Cmd.Exec': ({ fn, args }) => pure(Cmd.Exec({ fn, args })),
     'Cmd.Run': ({ expr }) => transformer.Expr(expr).map(Cmd.Run),
     'Cmd.Def': ({ variable, value }) =>
       transformer.Expr(value).map((value) =>
@@ -54,7 +53,7 @@ export const transformM = (of: <T>(t: T) => Monad<T>) => {
       ),
     'Cmd.Return': ({ expr }) => transformer.Expr(expr).map(Cmd.Return),
     'Cmd.Dialogue': ({ character, line }) =>
-      of(Cmd.Dialogue({ character, line })),
+      pure(Cmd.Dialogue({ character, line })),
     'Cmd.ChooseOne': ({ branches }) =>
       sequenceM<ChoiceBranch>(branches.map(transformer.Branch)).map(
         Cmd.ChooseOne
@@ -73,13 +72,13 @@ export const transformM = (of: <T>(t: T) => Monad<T>) => {
 
   // Expressions
   const visitExpr = (transformer: Transformer): ExprVisitor => ({
-    'Expr.Import': ({ path }) => of(Expr.Import(path)),
+    'Expr.Import': ({ path }) => pure(Expr.Import(path)),
     'Expr.Eval': ({ fn, args }) =>
       sequenceM<Expr>(args.map(transformer.Expr)).map((args) =>
         Expr.Eval({ fn, args })
       ),
-    'Expr.Var': ({ variable }) => of(Expr.Var(variable)),
-    'Expr.Lit': ({ value }) => of(Expr.Lit(value)),
+    'Expr.Var': ({ variable }) => pure(Expr.Var(variable)),
+    'Expr.Lit': ({ value }) => pure(Expr.Lit(value)),
     'Expr.Template': ({ parts }) =>
       sequenceM<Expr>(parts.map(transformer.Expr)).map(Expr.Template),
     'Expr.Unary': ({ op, expr }) =>
@@ -115,7 +114,7 @@ export const transformM = (of: <T>(t: T) => Monad<T>) => {
       const condM = transformer.Expr(condition)
       const resultM = transformer.Expr(result)
       return condM.flatMap((condition) =>
-        resultM.flatMap((result) => of(Branch.Cond({ condition, result })))
+        resultM.flatMap((result) => pure(Branch.Cond({ condition, result })))
       )
     },
   })
@@ -139,21 +138,32 @@ export const transformM = (of: <T>(t: T) => Monad<T>) => {
         match(branch, {
           ...visitBranch(transducer),
           ...(visitor.Branch || {}),
-        }).map((cmdResult) => ({
+        }).map((branchResult) => ({
           ...(branch.loc ? { loc: branch.loc } : {}),
-          ...cmdResult,
+          ...branchResult,
         })),
     }
     return transducer
   }
 
+  return makeTransformer
+}
+
+export const transformM = (pure: <T>(t: T) => Monad<T>) => {
+  const sequenceM = <T>(arrM: Monad<T>[]): Monad<T[]> =>
+    arrM.reduce(
+      (arrM, valM) =>
+        arrM.flatMap((arr) => valM.flatMap((val) => pure([...arr, val]))),
+      pure([] as T[])
+    )
+
   // Program
   const runTransformM =
     (visitor: ASTVisitor) =>
     ({ commands }: Prog): Monad<Prog> => {
-      const transformer = makeTransformer(visitor)
+      const transformer = visitM(pure)(visitor)
       return sequenceM<Cmd>(commands.map(transformer.Cmd)).flatMap((commands) =>
-        of({ commands })
+        pure({ commands })
       )
     }
 
